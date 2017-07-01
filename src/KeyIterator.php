0c8915b093c77f8c09670eb7b4c16faa357d6aca
Bart Feenstra <bart@mynameisbart.com>
2017-07-01 14:30:47 +0200

    Add keys(), to convert an iterator to return its keys.

---
+++ src/KeyIterator.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace BartFeenstra\Functional;
+
+/**
+ * Iterates over keys.
+ */
+final class KeyIterator extends \IteratorIterator implements Iterator
+{
+
+    use IteratorTrait;
+
+    private $index = 0;
+
+    public function current()
+    {
+        return parent::key();
+    }
+
+    public function key()
+    {
+        return $this->index;
+    }
+
+    public function next()
+    {
+        parent::next();
+        $this->index++;
+    }
+
+    public function rewind()
+    {
+        parent::rewind();
+        $this->index = 0;
+    }
+}
